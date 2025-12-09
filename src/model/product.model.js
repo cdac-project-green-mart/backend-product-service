403f6fdc88000510f19fcbf097e75dc6790b1fc4
atharvams <atharvams009@gmail.com>
2025-12-09 17:08:56 +0530

    task: product model added.

---
+++ src/model/product.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    images: [String],
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", ProductSchema);
