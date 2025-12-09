45867c49f9c9d64a59b697e7d28bb445248ec302
atharvams <atharvams009@gmail.com>
2025-12-09 16:02:56 +0530

    fix: 2 fields added into env config.

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -5,4 +5,6 @@ export const envConfig = {
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/products",
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || "dev",
+  MONGO_USERNAME: process.env.MONGO_USERNAME || "admin",
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD || "password",
 };
